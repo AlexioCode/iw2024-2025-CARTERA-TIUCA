61f400bf6b06378c4d9115315116c6dedd37cada
Juan Carlos <juancarlos.fernandezpina@alum.uca.es>
2024-11-07 18:59:24 +0100

    Revert "Datos añadidos"
    This reverts commit 7b3b2712fdcb3b3285db6fb48a3a967c28e1551c.

--- cartera-tiuca/src/main/java/com/example/carteratiuca/data/Estado.java
+++
@@ -1,6 +0,0 @@
-package com.example.carteratiuca.data;
-
-public enum Estado{
-    REGISTRADO, ADMITIDO, POSPUESTO, NO_TERMINADO, DENEGADO;
-}
-
--- cartera-tiuca/src/main/java/com/example/carteratiuca/data/Role.java
+++ cartera-tiuca/src/main/java/com/example/carteratiuca/data/Role.java
@@ -1,6 +1,5 @@
 package com.example.carteratiuca.data;
 
 public enum Role {
-    USER, ADMIN, SOLICITANTE, PROMOTOR, OTP, CIO;
+    USER, ADMIN;
 }
-
