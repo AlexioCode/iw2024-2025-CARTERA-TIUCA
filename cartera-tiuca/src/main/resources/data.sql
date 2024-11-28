dac0aa77ccf383ee4817793cdb0b8ecc5573ea5e
John Michaell Ramos Vera <john.ramosvera@alum.uca.es>
2024-11-28 16:40:38 +0100

    añadido email a data.sql

--- cartera-tiuca/src/main/resources/data.sql
+++ cartera-tiuca/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into carteratiuca.app_users (version, id, username,password,profile_picture, unit) values (1, '1','user','$2a$10$xdbKoM48VySZqVSU/cSlVeJn0Z04XCZ7KZBjUBC00eKo5uLswyOpe','', 'unidad1');
+insert into carteratiuca.app_users (version, id, username,password,profile_picture, unit, email) values (1, '1','user','$2a$10$xdbKoM48VySZqVSU/cSlVeJn0Z04XCZ7KZBjUBC00eKo5uLswyOpe','', 'unidad1', 'emailprueba@uwu.es');
 insert into user_roles (user_id, roles) values ('1', 'USER');
-insert into carteratiuca.app_users (version, id, username,password,profile_picture, unit) values (1, '2','admin','$2a$10$jpLNVNeA7Ar/ZQ2DKbKCm.MuT2ESe.Qop96jipKMq7RaUgCoQedV.','', 'unidad2');
+insert into carteratiuca.app_users (version, id, username,password,profile_picture, unit, email) values (1, '2','admin','$2a$10$jpLNVNeA7Ar/ZQ2DKbKCm.MuT2ESe.Qop96jipKMq7RaUgCoQedV.','', 'unidad2', 'emailprueba2@uwu.es');
 insert into user_roles (user_id, roles) values ('2', 'USER');
 insert into user_roles (user_id, roles) values ('2', 'ADMIN');
