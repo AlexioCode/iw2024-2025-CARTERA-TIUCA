731961b412d66b6a7d4b3e1b3101fcb2305c89ef
Juan Carlos <juancarlos.fernandezpina@alum.uca.es>
2025-01-04 13:46:01 +0100

    avance clase convocatoria

--- cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/views/AdminConvocatoriaManageView.java
+++ cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/views/AdminConvocatoriaManageView.java
@@ -1,12 +1,17 @@
 package es.uca.iw.carteratiuca.views;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import es.uca.iw.carteratiuca.entities.Convocatoria;
 import es.uca.iw.carteratiuca.services.ConvocatoriaService;
 import jakarta.annotation.security.RolesAllowed;
+
+import java.util.List;
 
 @PageTitle("Todas las convocatorias")
 @Route("admin-convocatoria-manage")
@@ -20,7 +25,28 @@ public class AdminConvocatoriaManageView extends Composite<VerticalLayout> {
     public AdminConvocatoriaManageView(ConvocatoriaService service) {
         this.service = service;
 
-        
+        H3 h3 = new H3();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        h3.setText("Convocatorias");
+        h3.setWidth("max-content");
+        getContent().add(h3);
+
+        List<Convocatoria> allConvos = service.getConvocatorias();
+
+        Grid<Convocatoria> projectGrid = new Grid<>();
+        getContent().getStyle().set("flex-grow", "1");
+        projectGrid.addColumn(Convocatoria::getFechaInicial).setHeader("Fecha Inicial").setSortable(true);
+        projectGrid.addColumn(Convocatoria::getFechaFinal).setHeader("Fecha Final").setSortable(true);
+
+
+        setGridData(projectGrid);
+        getContent().add(projectGrid);
+
     }
 
+
+    private void setGridData(Grid grid) {
+        grid.setItems(service.getConvocatorias());
+    }
 }
