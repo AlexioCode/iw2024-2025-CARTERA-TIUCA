098f70dbe601d5bc86c37b41adc24cd74c5a9af1
Alejandro Alonso-Basurto <alejandro.alondiaz@alum.uca.es>
2025-01-09 11:16:17 +0100

    Consumición de servicio REST arreglada (único punto de entrada)

--- cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/Application.java
+++ cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/Application.java
@@ -2,21 +2,32 @@ package es.uca.iw.carteratiuca;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import es.uca.iw.carteratiuca.servicitoRest.ConsumirServicioWeb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "cartera-tiuca")
-public class Application implements AppShellConfigurator {
+public class Application implements AppShellConfigurator, CommandLineRunner {
+
+    @Autowired
+    ConsumirServicioWeb consumirServicioWeb;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        consumirServicioWeb.ejecutar(new RestTemplate());
     }
 }
--- cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/servicitoRest/ConsumirServicioWeb.java
+++ cartera-tiuca/src/main/java/es/uca/iw/carteratiuca/servicitoRest/ConsumirServicioWeb.java
@@ -5,59 +5,43 @@ import es.uca.iw.carteratiuca.entities.User;
 import es.uca.iw.carteratiuca.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.UUID;
 
-@SpringBootApplication
+@Service
 public class ConsumirServicioWeb {
     private static final Logger log =
             LoggerFactory.getLogger(ConsumirServicioWeb.class);
 
-    private UserService userService;
+    private final UserService userService;
 
-    public ConsumirServicioWeb (UserService userService) { this.userService = userService; }
-
-    public static void main(String[] args) {
-        SpringApplication.run(ConsumirServicioWeb.class, args);
+    public ConsumirServicioWeb(UserService userService) {
+        this.userService = userService;
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Json json = restTemplate.getForObject(
-                    "https://e608f590-1a0b-43c5-b363-e5a883961765.mock.pstmn.io/sponsors", Json.class
-            );
-            log.info(json.toString());
-            int contandor = 0;
-            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            for (Promotor currentPromotor : json.getData())
-            {
-                User nuevoPromotor = new User();
-                nuevoPromotor.setId(UUID.randomUUID());
-                nuevoPromotor.setUsername(currentPromotor.getNombre());
-                nuevoPromotor.setCargo(currentPromotor.getCargo());
-                nuevoPromotor.setActive(true);
-                nuevoPromotor.setEmail("GeneradorDeEmail" + contandor + "@uwu.com" );
-                nuevoPromotor.setPassword("12345678");
-                nuevoPromotor.addRole(Role.PROMOTOR);
-                nuevoPromotor.setUnit("unidad" + contandor);
-                userService.registerUser(nuevoPromotor);
-                contandor++;
-            }
-        };
+    public void ejecutar(RestTemplate restTemplate) throws Exception {
+        Json json = restTemplate.getForObject(
+                "https://e608f590-1a0b-43c5-b363-e5a883961765.mock.pstmn.io/sponsors", Json.class
+        );
+        log.info(json.toString());
+        int contandor = 0;
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        for (Promotor currentPromotor : json.getData()) {
+            User nuevoPromotor = new User();
+            nuevoPromotor.setId(UUID.randomUUID());
+            nuevoPromotor.setUsername(currentPromotor.getNombre());
+            nuevoPromotor.setCargo(currentPromotor.getCargo());
+            nuevoPromotor.setActive(true);
+            nuevoPromotor.setEmail("GeneradorDeEmail" + contandor + "@uwu.com");
+            nuevoPromotor.setPassword("12345678");
+            nuevoPromotor.addRole(Role.PROMOTOR);
+            nuevoPromotor.setUnit("unidad" + contandor);
+            userService.registerUser(nuevoPromotor);
+            contandor++;
+        }
     }
 }
